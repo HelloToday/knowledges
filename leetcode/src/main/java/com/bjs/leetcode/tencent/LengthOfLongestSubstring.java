506ceeda51730e1be726d7fee4ecb17cc53491c9
秋意浓 <1124924711@qq.com>
2018-10-07 21:33:56 +0800

    1.需要考虑有重复字符的情况 2 考虑没有重复字符的情况 3边界值

--- leetcode/src/main/java/com/bjs/leetcode/tencent/LengthOfLongestSubstring.java
+++ leetcode/src/main/java/com/bjs/leetcode/tencent/LengthOfLongestSubstring.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 public class LengthOfLongestSubstring {
     public static void main(String[] args){
 
-        String ss = "pwwkew";
+        String ss ="abc";
         int result = lengthOfLongestSubstring(ss);
         System.out.println(result);
 
@@ -19,26 +19,26 @@ public class LengthOfLongestSubstring {
      * @return
      */
     public static  int lengthOfLongestSubstring(String s) {
-        if(s==null||"".equals(s.trim())){
+        if(s==null||"".equals(s)){
             return 0;
         }
         int length = 0;
         char[] chars = s.toCharArray();
         for (int i = 0; i <chars.length ; i++) {
             HashSet<Character> set = new HashSet<>();
-            char charstr = chars[i];
             for (int j = i; j <chars.length ; j++) {
                 if(!set.contains(chars[j])){
                     set.add(chars[j]);
-                    System.out.println(chars[j]);
+
+                    if(j==chars.length-1&&set.size()>length){
+                        length = set.size();
+                    }
                 }else{
-                    System.out.println("equal "+chars[j]);
                     if(set.size()>length){
                         length = set.size();
                     }
                     break;
                 }
-
             }
 
             set.clear();
